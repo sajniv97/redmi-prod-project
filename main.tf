60dd1526d1355ee7221af473a3100da5838fe4d3
sajni <sajniv97@gmail.com>
2023-12-07 11:56:36 +0000

    monitoring port 8080 added

--- main.tf
+++ main.tf
@@ -30,6 +30,14 @@ resource "aws_security_group" "web-access" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+  ingress {
+    from_port        = 8080
+    to_port          = 8080
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
 
 
   egress {
